4f7981e5acc05895ee9c625521e9fa7de117ad28
Benjamin Wiegand <ben@WL-POOL4-PNT-02014.UNI-MUENSTER.DE>
2016-03-08 14:05:07 +0100

    better interpolatino

--- caustic_beams.py
+++ caustic_beams.py
@@ -25,22 +25,23 @@ ORDER = 4
 # the corresponding axis
 LIMITS = [
     (-50,50),   # a axis
-    (-50,15),   # b axis
-    #(-10,)*2,  # c axis
-    #(-10,)*2,  # d axis
+    (-50,50),   # b axis
+    #(0,)*2,  # c axis
+    #(0,)*2,  # d axis
 ]#
 # number of data points for each axis
-RESOLUTION = [300,300]
+RESOLUTION = [200,200]
 # number of weights for legendre gauss integration
-NUM_WEIGHTS = 400
+NUM_WEIGHTS = 500
 # neglected integral is proportional to exp(-D)
 D = 100
 # a grid of N_ROOTS**2 roots is calculated and then interpolated.
 # N_ROOTS=25 should be enough
 N_ROOTS = 25
-# in case of pearcey beam: should the results of method of steepest descent
-# and integration method be merged together? This reduces the number of weights
-# needed. Otherwise, only integration method is used.
+# in case of pearcey beam:
+# should the results of method of steepest descent and integration method be
+# merged together? This reduces the number of weights needed.
+# Otherwise, only integration method is used.
 USE_MASK = True
 
 # ==================================================
@@ -66,7 +67,7 @@ t = time()
 var = []
 S = sp.Symbol('S')
 
-catastrophe = S ** ORDER
+catastrophe = S ** (ORDER)
 
 for i in xrange(ORDER - 2):
     tmp_var = sp.Symbol(lowercase[i])
@@ -81,12 +82,12 @@ expr_ddd = sp.diff(expr_dd, S)
 if ORDER == 4:
     # find saddle points (dep. of S)
     saddles_s = sp.solve(expr_d, S)
-
+    
     # calculate the value of the function and its derivatives at each saddle point
     def _plug_in(expr, S, saddle_s, var):
         expr = expr.subs(S, saddle_s)
         return sp.lambdify(var, expr, [{'ImmutableMatrix': np.array}, 'numpy'])
-
+    
     val = [_plug_in(catastrophe, S, saddle_s, var) for saddle_s in saddles_s]
     d = [_plug_in(expr_d, S, saddle_s, var) for saddle_s in saddles_s]
     dd = [_plug_in(expr_dd, S, saddle_s, var) for saddle_s in saddles_s]
@@ -129,19 +130,34 @@ range_fine = [np.linspace(*l) for l in PLOT_LIMITS]
 # generade coarse grid for root calculation
 slices2 = [slice(lim[0], lim[1], res > 1 and (N_ROOTS*1j) or 1j) for lim, res in zip(LIMITS, RESOLUTION)]
 grid_coarse = np.mgrid[slices2].astype(np.float)
+cp = copy(grid_coarse)
 range_coarse = [np.linspace(l[0], l[1], N_ROOTS) for l in PLOT_LIMITS]
 
 def _get_coeff(pos, neg=False):
-    if neg and (ORDER % 2 != 0):
+    # factor that determines whether integration takes place below or above the
+    # real axis in complex space
+    if neg and ((ORDER) % 2 != 0):
         fac = -1
     else:
         fac = 1
-    fac2 = neg and -1 or 1
 
-    coeff = [-D]
+    # prefactor that determines whether integration takes place towars inf or
+    # -inf
+    pref = neg and -1 or 1
+    
+    coeff = np.zeros(ORDER + 1)
+    
+    # zeroth order
+    coeff[-1] = -D
+
     for i in xrange(ORDER - 2):
-        coeff = [(fac2 ** (i+1)) * pos[i] * np.sin(fac * (i+1) * np.pi / (2*ORDER))] + coeff
-    coeff = [1, 0] + coeff
+        order = (i+1)
+        idx = -order - 1
+        coeff[idx] = (pref ** order) * pos[i] * \
+            np.sin(fac * order * np.pi / (2*ORDER))
+
+    # highest order    
+    coeff[0] = 1
     return coeff
 
 # calculate a coarse grid of roots
@@ -155,10 +171,10 @@ roots_coarse = np.squeeze(grid_coarse[0])
 roots_coarse_neg = np.squeeze(grid_coarse[1])
 
 # create fine grid for the roots by means of interpolation
-f = interpolate.interp2d(range_coarse[0], range_coarse[1], roots_coarse, kind='linear')
-f2 = interpolate.interp2d(range_coarse[0], range_coarse[1], roots_coarse_neg, kind='linear')
-roots_fine = f(range_fine[1], range_fine[0])
-roots_fine_neg = f2(range_fine[1], range_fine[0])
+f = interpolate.RectBivariateSpline(range_coarse[0], range_coarse[1], roots_coarse)
+f2 = interpolate.RectBivariateSpline(range_coarse[0], range_coarse[1], roots_coarse_neg)
+roots_fine = f(range_fine[0], range_fine[1])
+roots_fine_neg = f2(range_fine[0], range_fine[1])
 
 # we have to expand the dimension of the grid again
 for i, l in enumerate(LIMITS):
@@ -183,7 +199,7 @@ for s, w in zip(s_values, weights):
     mat_neg += w * caustic_beam(-s * roots_fine_neg, *grid_fine)
 
     # integration from R1 along the arc
-    s_val = roots_fine * np.exp(1j * s * angle)
+    s_val = roots_fine * np.exp(1j * s * angle)    
     mat2 += w * caustic_beam(s_val, *grid_fine)
 
     # integration from -R2 along the arc
@@ -216,35 +232,35 @@ if ORDER == 4:
         limits_s[SYMMETRY] = (limits_s[SYMMETRY][0], 0)
         assert RESOLUTION[SYMMETRY] % 2 == 0, 'Resolution in symmetry direction has to be even'
         resolution_s[SYMMETRY] = resolution_s[SYMMETRY] / 2
-
-
+    
+    
     def _saddle_contribution(val, dd, saddle_i, *positions):
         """
         Calculates the contribution to the E-field of each saddle.
         """
         val = val[saddle_i](*positions)
         deriv = dd[saddle_i](*positions)
-
+    
         ang = np.angle(deriv)
         theta_ = (np.pi - ang) / 2
 
         if saddle_i == 1:
             theta_[theta_ > np.pi/2] -= np.pi
-
+    
         return np.exp(1j * (val + theta_)) * np.sqrt(2*np.pi/abs(deriv))
-
+    
     # create the grid
     slices = [slice(lim[0], lim[1], res*1j) for lim, res in zip(limits_s, resolution_s)]
     positions = np.mgrid[slices].astype(complex)
-
+    
     # calculate E field
     E_s = sum(_saddle_contribution(val, dd, i, *positions) for i,s in enumerate(saddles_s))
     E_s[np.isnan(E_s)] = 0
-
+    
     # use symmetry
     stack = (PLOT_AXES.index(SYMMETRY) == 0) and np.vstack or np.hstack
     E_s = stack((E_s, np.flipud(E_s))).T
-
+    
     print '%f seconds' % (time() - t)
 
 #%%
@@ -261,7 +277,7 @@ for i, (lim, res) in enumerate(zip(LIMITS, RESOLUTION)):
 
 if USE_MASK and ORDER == 4:
     mask = np.zeros(E_s.shape)
-
+    
     # position of the caustic y=f(x)
     f = lambda x: -(27.0/8*(abs(x)+CAUSTIC_DISTANCE)**2)**(1.0/3)
 
@@ -285,15 +301,14 @@ ang = np.angle(E)
 
 plt.clf()
 
-def _plot(mat, vmax, i):
+def _plot(mat, vmin, vmax, i):
     plt.subplot(2, 1, i)
-    plt.pcolormesh(axes[0], axes[1], mat, vmax=vmax)
+    plt.pcolormesh(axes[0], axes[1], mat, vmax=vmax, vmin=vmin)
     plt.colorbar()
     plt.xlim(min(axes[0]), max(axes[0]))
     plt.ylim(min(axes[1]), max(axes[1]))
     plt.xlabel(lowercase[PLOT_AXES[0]])
     plt.ylabel(lowercase[PLOT_AXES[1]])
 
-_plot(I, 6, 1)
-_plot(ang, np.pi, 2)
-plt.show()
+_plot(I, 0, 6, 1)
+_plot(ang, -np.pi, np.pi, 2)
